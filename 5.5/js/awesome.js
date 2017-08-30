ed7a36ba4a4123e1d0de6a213fa262cfea21f6f8
tstrain199 <tstrain199@gmail.com>
2017-08-29 20:15:47 -0400

    Added counter

--- 5.5/js/awesome.js
+++ 5.5/js/awesome.js
@@ -4,6 +4,7 @@
 // document.body.style.background="white";
 
 var nums = [1,2,3,4,5];
+var clicks = [0,0,0,0,0];
 var cats = ["Bags", "Barron", "Dexter", "Parker", "Stanley"];
 var pics = ["img/bags.jpg", "img/barron.jpg", "img/dexter.jpg", "img/parker.jpg", "img/stanley.jpg"];
 
@@ -12,6 +13,7 @@ $(".col-10").append('<image src="' + pics[0] +'">');
 for (i = 0; i < cats.length; i++) {
   var cat = cats[i];
   var pic = pics[i];
+  var click = clicks[i];
   //var elem = $(".list").append('<li>' + cat + '</li>');
   var x = document.createElement("LI");
   var t = document.createTextNode(cat);
@@ -19,11 +21,13 @@ for (i = 0; i < cats.length; i++) {
   document.getElementById("myList").appendChild(x);
 
   //elem.on('click',(function(j){
-  x.addEventListener('click', (function(picCopy) {
+  x.addEventListener('click', (function(picCopy, clickCopy) {
      return function() {
        var src = $(this).attr('src');
        $('.col-10 img').attr('src', picCopy);
-       console.log(picCopy);
+       clickCopy +=1;
+       $('.counter').text(clickCopy);
+       console.log(clickCopy);
      };
-  })(pic));
+  })(pic, click));
 };
